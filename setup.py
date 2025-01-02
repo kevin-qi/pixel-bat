fb9d5e3fd1c2a75047bc3bd93b2c29b72775d886
Kevin Qi <kevin.qi@berkeley.edu>
2025-01-01 22:51:13 -0800

    Fix improper installation path bug

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="pixelbat",
     version="0.1.0",
-    packages=find_packages(where="pixelbat", include=["*"]),
-    package_dir={"": "pixelbat"},
+    packages=find_packages(),
     install_requires=[
         "numpy",
         "matplotlib",
